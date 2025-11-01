eedd914d4096c638bca2f7f0b186e6f1a1c38d10
FX. Mario Jevta Prasetio <fxmariojevta@gmail.com>
2025-11-01 15:03:13 +0700

    feat: handle go routines with sync wait group

--- main.go
+++ main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething() {
-	fmt.Println("Something")
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
 }
 
 func main() {
-	go printSomething()
+	var wg sync.WaitGroup
 
-	time.Sleep(1 * time.Second)
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"theta",
+		"epsilon",
+	}
 
-	fmt.Println("Something in main")
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d : %s", i, x), &wg)
+	}
+
+	wg.Wait()
+	wg.Add(1)
+
+	printSomething("Something after words", &wg)
 }
